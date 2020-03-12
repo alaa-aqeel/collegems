ac1ae56e52c5100326f10d18228904ffe6b4d8ab
alaaProg <alaa.21.iraq@gmail.com>
2020-03-12 20:24:30 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,48 +31,52 @@
             <!-- Demo content -->
                 <!--end header-->
                 @if(!Auth::user()->active && Auth::user()->email_verified_at )
-                    <div class="row text-ceneter">
-                        <h5 class='text-secondary'> Your Account need active from your college. wait to 24hours plase to active  </h5>
+                    <div class="text-ceneter">
+                        <h5 class='text-secondary'>
+                            Your Account need active from your college. wait to 24hours plase to active
+                        </h5>
                     </div>
                 @endif
                 <div class="row">
                     @if($projects)
-                        <p>- Project College {{ $projects->count() }}</p>
-                        <hr>
-                        @foreach ($projects as $project)
-                            <div class="card tranining">
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-between pb-4">
-                                        <p class="text">
-                                            {{ $project->name }}
-                                            <small style="color:#999595">
-                                                <i class="fa fa-calendar-minus-o" aria-hidden="true"></i>
-                                                {{ date('Y-m-d', strtotime($project->created_at)) }}
-                                            </small>
-                                        </p>
-                                        {{-- <label class="check-box">
-                                            <input type="checkbox" />
-                                            <span  style="font-size: 1.1rem;color:#e6d12b;">
-                                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                                            </span>
-                                        </label> --}}
+                        <div class="col-12">
+                            <p>- Project College {{ $projects->count() }}</p>
+                            {{-- <hr> --}}
+                            @foreach ($projects as $project)
+                                <div class="card tranining">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between pb-4">
+                                            <p class="text">
+                                                {{ $project->name }}
+                                                <small style="color:#999595">
+                                                    <i class="fa fa-calendar-minus-o" aria-hidden="true"></i>
+                                                    {{ date('Y-m-d', strtotime($project->created_at)) }}
+                                                </small>
+                                            </p>
+                                            {{-- <label class="check-box">
+                                                <input type="checkbox" />
+                                                <span  style="font-size: 1.1rem;color:#e6d12b;">
+                                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                                </span>
+                                            </label> --}}
+                                        </div>
+                                        <article>
+                                            {{ $project->description }}
+                                            <a href="{{ $project->link }}">github</a>
+                                        </article>
+
+                                        <div class="card-footer"></div>
                                     </div>
-                                    <article>
-                                        {{ $project->description }}
-                                        <a href="{{ $project->link }}">github</a>
-                                    </article>
-
-                                    <div class="card-footer"></div>
+                                    <div class="card-text card-show pt-4 ">
+                                        @foreach($project->users as $user)
+                                            <a href="/user/{{ $user->id }}">{{'@'.$user->fullname}}</a>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <div class="card-text card-show pt-4 ">
-                                    @foreach($project->users as $user)
-                                        <a href="/user/{{ $user->id }}">{{'@'.$user->fullname}}</a>
-                                    @endforeach
-                                </div>
-                            </div>
-                        @endforeach
+                            @endforeach
 
-                        {{ $projects }}
+                            {{ $projects }}
+                        </div>
                     @else
                     <div class="text-center w-100">
                         <h1 > College Have not projects </h1>
