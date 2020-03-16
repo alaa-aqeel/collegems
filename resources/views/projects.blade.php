1bbf7e78a7ca94daae14f3b59a7838183368e643
alaaProg <alaa.21.iraq@gmail.com>
2020-03-16 10:27:41 +0300

    up

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -45,19 +45,38 @@
                                     </a>
                                 </h4>
                                 <p class="card-text w-100" style='float: unset;margin-top:0'>
-                                    {{ $project->description }}
+                                    - <a href="#!" class="btn btn-link" data-toggle="modal" data-target="#des{{ $project->id }}cription">
+                                        Description
+                                    </a>
                                     <br>
-                                    - <strong style="text-transform: capitalize">{{ $project->college->name }}</strong>
+                                    - <strong style="text-transform: capitalize">
+                                        {{ $project->college->name }}
+                                    </strong>
                                     <br>
                                     @foreach($project->users as $user)
                                         <strong style="color:#00000078">
-                                            <a href="/user/{{  $user->id }}" class="code mt-5" >
+                                            <a href="/user/{{  $user->username }}" class="code mt-5" >
                                                 {{ '@'.$user->fullname }}
                                             </a>
                                         </strong>
                                     @endforeach
                                     <a href="{{ $project->link }}" class="code float-right"> <i class="fa fa-github fa-2x"></i> </a>
                                 </p>
+                                <div class="modal"  id='des{{ $project->id }}cription' tabindex="-1" role="dialog">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">{{ $project->name }}</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>{{ $project->description }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
